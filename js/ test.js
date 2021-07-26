7ab5e558065abc96874f5a478312ddc1e76b7eaa
yelenchuk <t.elenchuk19@gmail>
2021-07-26 17:22:57 +0300

    lesson 69 encapsulation on class

--- js/ test.js
+++ js/ test.js
@@ -1,33 +1,40 @@
 "use strict";
 
-function User(name, age) {
-  this.name = name;
-  let userAge = age; // encapsulation
+class User {
+  constructor(name, age) {
+    this.name = name;
+    //let userAge = age; // encapsulation
+    //this.userAge = age; // сделали св-во публичным
+    this._age = age; // сделали св-во приватным encapsulation
+  }
 
-  this.say = function () {
-    console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`);
+  //surname = "Ivanov"; // св-во объекта вне конструктора
+  surname = "Ivanov"; // # делает св-во приватным
+
+  // методы можем задать в качестве => чтобы не терять контекст(контекстом будет определенный экземпляр класса - новый объект)
+  say = () => {
+    console.log(
+      `Имя пользователя: ${this.name}${this.surname}, возраст ${this._age}`
+    );
   };
 
-  // используем методы сеттеры и геттеры
-  this.getAge = function () {
-    return userAge;
-  };
+  // используем название св-ва аксессора
+  get age() {
+    return this._age;
+  }
 
-  this.setAge = function (age) {
+  // используем название св-ва аксессора
+  set age(age) {
     if (typeof age === "number" && age > 0 && age < 110) {
-      userAge = age;
+      this._age = age;
     } else {
       console.log("Недопустимое значение!");
     }
-  };
+  }
 }
 const ivan = new User("Ivan", 27);
-console.log(ivan.name);
-console.log(ivan.getAge());
-
-ivan.setAge(30);
-ivan.setAge(300);
-console.log(ivan.getAge());
-ivan.name = "Alex";
-
+// console.log(ivan.age); // get
+// ivan.age = 99; // set
+// console.log(ivan.age);
+console.log(ivan.sername); // undefined
 ivan.say();
